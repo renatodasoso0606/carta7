b38d5bbfd2c87b3165d9089bd00e0738dd2bb7d6
renatodasoso0606 <164267899+renatodasoso0606@users.noreply.github.com>
2024-06-11 23:40:41 -0300

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Você vai ser minha namorada pra sempre?",
+  descriptionHeader:
+    "Se você sair e não responder, isso significa que você já aceitou ^^",
+  buttonYes: "Aceito.",
+  buttonNo: "Não.",
+  titleModar: "Obrigado por aceitar",
+  descriptionModar:
+    'Eu sabia que você sempre vai ser meu amorzinho ^^ </br> Obrigado por ter começado a conversar comigo há um tempo atrás. Realmente não sei o quão diferente e ruim estaria minha vida sem você. </br> "Você muda tudo, e tudo fica tão bem Mil cores, melhores amigos Nós não vamos mais ser tão sozinhos, bebê Conto com você, pode contar comigo" </br> "When I see your face There\'s not a thing that I would change \'Cause you\'re amazing Just the way you are And when you smile The whole world stops and stares for a while \'Cause girl, you\'re amazing Just the way you are"',
+};
